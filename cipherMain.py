ef657fd1d4d31d3603a5a5a88eb1c2d9f4a9dc8a
jack-robs <jmrobertson@protonmail.com>
2020-03-10 21:54:42 -0400

    added main docstring

--- cipherMain.py
+++ cipherMain.py
@@ -2,6 +2,12 @@ import caesar_encoder
 
 
 def main():
+    '''
+    Caesar cipher
+    Has tests
+    encodes with monoalphabetic numeric shift 
+    '''
+
     cipherText = caesar_encoder.processor()
     print("Cipher in main:", cipherText)
 
